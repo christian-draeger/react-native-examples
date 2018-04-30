f120d64c89744b30b03e92bebb0907290953d0be
Christian Dräger <christian.draeger1@gmail.com>
2018-04-30 18:00:57 +0200

    accordion: add libraryList component that uses ListView; add ListItem component

--- accordion/App.js
+++ accordion/App.js
@@ -3,13 +3,15 @@ import {View} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from './src/reducers';
-import {Header} from './src/components/common'
+import {Header} from './src/components/common';
+import LibraryList from './src/components/LibraryList';
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
-            <View>
+            <View style={{ flex: 1 }}>
                 <Header headerText={"Tech Stack"} />
+                <LibraryList/>
             </View>
         </Provider>
     );
