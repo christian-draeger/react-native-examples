f9842283bf327bc9d7da08299a25d71dde47adc7
Christian Dräger <christian.draeger1@gmail.com>
2018-04-30 10:47:08 +0200

    auth: prototype text input

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
-import { Button, Card, CardSection} from "./common";
+import React, {Component} from 'react';
+import {TextInput} from 'react-native';
+import {Button, Card, CardSection} from "./common";
 
 class LoginForm extends Component {
+
+    state = {text: ''};
+
     render() {
         return (
             <Card>
-                <CardSection/>
+                <CardSection>
+                    <TextInput
+                        value={this.state.text}
+                        onChangeText={text => this.setState({text})}
+                        style={{height: 20, width: 100}}
+                    />
+                </CardSection>
                 <CardSection/>
 
                 <CardSection>
