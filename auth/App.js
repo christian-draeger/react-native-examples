2731b7aa3fa0a7b55b10f7887deb9132e9a0a190
Christian Dräger <christian.draeger1@gmail.com>
2018-04-30 10:36:58 +0200

    auth: add Login form dummy

--- auth/App.js
+++ auth/App.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
-import { Text, View } from 'react-native';
-import { Header } from './src/components/common'
+import {View} from 'react-native';
+import firebase from 'firebase';
+import {Header} from './src/components/common';
+import LoginForm from './src/components/LoginForm'
 
 export default class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyCmqlmwxg-MFDl-DVU_SlM3UGQZX1VjX5A",
+            authDomain: "auth-example-7162a.firebaseapp.com",
+            databaseURL: "https://auth-example-7162a.firebaseio.com",
+            projectId: "auth-example-7162a",
+            storageBucket: "auth-example-7162a.appspot.com",
+            messagingSenderId: "515126508896"
+        });
+    }
+
     render() {
         return (
             <View>
-                <Header headerText={"Authentication"} />
-                <Text>An App</Text>
+                <Header headerText={"Authentication"}/>
+                <LoginForm/>
             </View>
         );
     }
